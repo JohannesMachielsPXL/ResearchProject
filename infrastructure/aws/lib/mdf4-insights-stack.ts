2603c19b397693a0a660894cc0810917f4b8b3ed
Dirk Moors <dirk@Dirks-MacBook-Pro.local>
2025-04-28 21:53:11 +0200

    Added athena and glue

--- infrastructure/aws/lib/mdf4-insights-stack.ts
+++ infrastructure/aws/lib/mdf4-insights-stack.ts
@@ -5,6 +5,8 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as logs from 'aws-cdk-lib/aws-logs';
+import * as glue from 'aws-cdk-lib/aws-glue';
+import * as athena from 'aws-cdk-lib/aws-athena';
 import {Construct} from 'constructs';
 import {BaseStack, Context} from "./base";
 import {createStackOutputParameter, getGlobalName} from "./common";
@@ -171,6 +173,182 @@ export class Mdf4InsightsStack extends BaseStack {
             stackName: id,
             description: 'Parquet Input Bucket Name',
             cfnOutput: true
+        });
+
+        const parquetOutputBucketQueryResults = new s3.Bucket(this, 'parquet-output-query-results', {
+            bucketName: `${mdfInputBucket.bucketName}-parquet-query-results`,
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: cdk.RemovalPolicy.RETAIN,
+        });
+
+        // Athena Access User
+        const athenaAccessUser = new iam.User(this, 'athena-access-user', {
+            userName: getGlobalName(ctx, 'athena-access-user'),
+        });
+
+        const athenaAccessKey = new iam.AccessKey(this, 'athena-access-key', {
+            user: athenaAccessUser,
+        });
+
+        // Athena Access Policy
+        new iam.Policy(this, 'athena-access-policy', {
+            policyName: getGlobalName(ctx, 'athena-access-policy'),
+            users: [athenaAccessUser],
+            document: new iam.PolicyDocument({
+                statements: [
+                    new iam.PolicyStatement({
+                        sid: 'AthenaQueryAccess',
+                        actions: [
+                            "athena:ListDatabases",
+                            "athena:ListDataCatalogs",
+                            "athena:ListWorkGroups",
+                            "athena:GetDatabase",
+                            "athena:GetDataCatalog",
+                            "athena:GetQueryExecution",
+                            "athena:GetQueryResults",
+                            "athena:GetTableMetadata",
+                            "athena:GetWorkGroup",
+                            "athena:ListTableMetadata",
+                            "athena:StartQueryExecution",
+                            "athena:StopQueryExecution",
+                        ],
+                        resources: ["*"],
+                    }),
+                    new iam.PolicyStatement({
+                        sid: 'GlueReadAccess',
+                        actions: [
+                            "glue:GetDatabase",
+                            "glue:GetDatabases",
+                            "glue:GetTable",
+                            "glue:GetTables",
+                            "glue:GetPartition",
+                            "glue:GetPartitions",
+                            "glue:BatchGetPartition",
+                        ],
+                        resources: ["*"],
+                    }),
+                    new iam.PolicyStatement({
+                        sid: 'AthenaLimitedS3AccessQueryResults',
+                        actions: [
+                            "s3:GetBucketLocation",
+                            "s3:GetObject",
+                            "s3:ListBucket",
+                            "s3:ListBucketMultipartUploads",
+                            "s3:ListMultipartUploadParts",
+                            "s3:AbortMultipartUpload",
+                            "s3:PutObject",
+                        ],
+                        resources: [
+                            `arn:aws:s3:::${parquetOutputBucketQueryResults.bucketName}`,
+                            `arn:aws:s3:::${parquetOutputBucketQueryResults.bucketName}/*`,
+                        ],
+                    }),
+                    new iam.PolicyStatement({
+                        sid: 'AthenaReadOnlyS3AccessDataLake',
+                        actions: [
+                            "s3:GetObject",
+                            "s3:ListBucket",
+                        ],
+                        resources: [
+                            `arn:aws:s3:::${parquetOutputBucket.bucketName}`,
+                            `arn:aws:s3:::${parquetOutputBucket.bucketName}/*`,
+                        ],
+                    }),
+                ],
+            }),
+        });
+
+        // Athena Workgroup
+        new athena.CfnWorkGroup(this, 'athena-workgroup', {
+            name: getGlobalName(ctx, 'athena-workgoup'),
+            description: `Athena workgroup ${ctx.stage}`,
+            state: 'ENABLED',
+            workGroupConfiguration: {
+                resultConfiguration: {
+                    outputLocation: `s3://${parquetOutputBucketQueryResults.bucketName}/`,
+                },
+                enforceWorkGroupConfiguration: true,
+            },
+        });
+
+        // Glue Role
+        const glueServiceRole = new iam.Role(this, 'AWSGlueServiceRoleDataLake', {
+            assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
+            roleName: getGlobalName(ctx, 'glue-service-role'),
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSGlueServiceRole')
+            ],
+            inlinePolicies: {
+                S3InputLimitedAccessPolicyAWSGlue: new iam.PolicyDocument({
+                    statements: [
+                        new iam.PolicyStatement({
+                            actions: ['s3:GetObject', 's3:ListBucket'],
+                            resources: [
+                                `arn:aws:s3:::${mdfInputBucket.bucketName}`,
+                                `arn:aws:s3:::${mdfInputBucket.bucketName}/*`,
+                            ],
+                        }),
+                        new iam.PolicyStatement({
+                            actions: ['s3:GetObject', 's3:PutObject', 's3:ListBucket'],
+                            resources: [
+                                `arn:aws:s3:::${parquetOutputBucket.bucketName}`,
+                                `arn:aws:s3:::${parquetOutputBucket.bucketName}/*`,
+                            ],
+                        }),
+                    ],
+                }),
+            }
+        });
+
+        // Glue Database
+        const parquetDatalakeDbName = getGlobalName(ctx, 'parquet-datalake-db').replace(/-/g, '_');
+        const parquetDatalakeDb = new glue.CfnDatabase(this, 'parquet-datalake-db', {
+            catalogId: ctx.deployment.account!,
+            databaseInput: {
+                name: parquetDatalakeDbName,
+                description: "Database for parquet data lake",
+            }
+        });
+
+        // Glue Job
+        const glueJob = new glue.CfnJob(this, 'glue-python-script-job', {
+            name: getGlobalName(ctx, 'create-glue-database-from-s3'),
+            role: glueServiceRole.roleArn,
+            glueVersion: '4.0',
+            workerType: 'G.1X',
+            numberOfWorkers: 2,
+            executionProperty: {maxConcurrentRuns: 1},
+            command: {
+                name: 'glueetl',
+                pythonVersion: '3',
+                scriptLocation: `s3://${mdfInputBucket.bucketName}/create-glue-database-from-s3-vG.3.0.py`,
+            },
+            defaultArguments: {
+                "--enable-auto-scaling": "true",
+                "--job-bookmark-option": "job-bookmark-disable",
+                "--databasename": parquetDatalakeDbName,
+                "--bucket_output": parquetOutputBucket.bucketName,
+                "--job-language": "python",
+                "--additional-python-modules": "boto3",
+            },
+            maxRetries: 0,
+            timeout: 2880,
+        });
+        glueJob.addDependency(parquetDatalakeDb);
+
+        // Glue Triggers
+        new glue.CfnTrigger(this, 'GlueJobTriggerOnDemand', {
+            name: getGlobalName(ctx, 'glue-trigger-manual'),
+            type: 'ON_DEMAND',
+            actions: [{jobName: glueJob.name!}],
+        });
+
+        new glue.CfnTrigger(this, 'GlueJobTriggerScheduled', {
+            name: getGlobalName(ctx, 'glue-trigger-scheduled'),
+            type: 'SCHEDULED',
+            schedule: 'cron(0 12 * * ? *)',
+            actions: [{jobName: glueJob.name!}],
+            startOnCreation: false,
         });
     }
 }
